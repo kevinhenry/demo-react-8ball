2a07eab380a7a3b205aab72799e300210005268c
Kevin Henry <kevinhenry@mac.com>
2021-09-28 21:18:32 -0700

    Initial Commit

---
+++ magic-eight-ball/data.js
@@ -0,0 +1,11 @@
+// https://en.wikipedia.org/wiki/Magic_8-Ball
+export const replies = [
+    "It is certain.",
+    "It is decidely so.",
+    "Without a doubt.",
+    "Yes - definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+
+]
